e1b81f412a04805823bfe0f31b08e79ade3c7ddd
sryan10 <masterseanm@gmail.com>
2017-09-14 14:53:17 -0700

    Made a Comment.

--- app/src/main/java/edu/orangecoastcollege/cs273/sryan10/occars/PurchaseActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/sryan10/occars/PurchaseActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class PurchaseActivity extends AppCompatActivity {
-
+//something different
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
